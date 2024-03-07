4043e0a9a6cf830c94b605965a18ba24a7025e77
amolmankar18 <117392544+amolmankar18@users.noreply.github.com>
2024-03-07 05:18:19 +0000

    changed drive file

--- src/components/Heroimage.js
+++ src/components/Heroimage.js
@@ -42,7 +42,7 @@ const Heroimage = () => {
         <p className="h5">11 June - 17 June 2024</p>
         <div>
           <a
-            href="https://drive.google.com/file/d/1fITQvxe9vvKBJqlm1cHCh-VhvFzhpl3T/view"
+            href="https://drive.google.com/file/d/1PoLVmFXQO0_uamxeYwYTglhOcU_sVieL/view"
             className="btn"
           >
             ITINERARY
