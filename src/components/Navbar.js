91a8389d52ad23f7fd60f597e5116a5db2118c06
Harsh Pathekar <96983175+pathekarharsh@users.noreply.github.com>
2026-02-03 15:48:30 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
+  // const handleClick = () => setClick(!click);
 
   const [color, setColor] = useState(false);
   const handleColor = () => {
